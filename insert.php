eeacb67d2cf1b0563f2701b936ad611b75d39161
Sudip Aryal <38017418+sa8123@users.noreply.github.com>
2020-04-30 00:05:09 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,28 +1,20 @@
 <?php
-	$connection = mysqli_connect('mysql.truman.edu', 'sa8123', 'vohxochi');
-	if(!$connection)
-	{
-		echo "Connection cannot be established";
-	}
-
-	if(!mysqli_select_db($connection, 'sa8123CS315'))
-	{
-		echo "Database cannot be selected";
-	}
-
+$connection = mysql_connect('mysql.truman.edu', 'sa8123', 'vohxochi'); // Establishing Connection with Server
+$db = mysql_select_db("sa8123CS315", $connection); // Selecting Database from Server
+if(isset($_POST['Insert']))
+{ 
 	$FName = $_POST['FirstName'];
 	$LName = $_POST['LastName'];
 	$Email = $_POST['Email'];
+	if($name !=''||$email !='')
+	{
 
-	// Name of the table is Information. Don't need to insert ID 
-	// since it is autoincremented. 
-	$sql = "INSERT INTO Information(FirstName, LastName, Email) VALUES ('$FNAME', '$LName', '$Email')";
-	if(!mysqli_query($connection, $sql))
-	{
-		echo "Data not inserted!";
-	}
-	else
-	{
-		echo "Data inserted!";
-	}
+	$query = mysql_query("insert into Information(Firstname, Lastname, Email) values ('$name', '$email', '$contact', '$address')");
+echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+}
+else{
+echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
+}
+}
+mysql_close($connection); 
 ?>
